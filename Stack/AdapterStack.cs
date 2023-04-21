d4e64773db1507a1d30e313589ac73c5baabfbd7
윤진 소 <pms010506@gmail.com>
2023-04-21 16:57:59 +0900

    feat: Stack

---
+++ Stack/AdapterStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class AdapterStack
+    {
+        private List<T> container;
+
+        public AdapterStack()
+        {
+            container = new List<T>();
+        }
+
+        public void Push(T item)
+        {
+            container.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = container[container.Count - 1];
+            container.RemoveAt(container.Count - 1);
+            return item;
+        }
+    }
+}
