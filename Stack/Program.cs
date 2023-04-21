6ae4280a89adbac1ead76f57b67675d5cc738dd1
윤진 소 <pms010506@gmail.com>
2023-04-21 17:04:02 +0900

    feat: Stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -2,29 +2,40 @@
 {
     internal class Program
     {
-        /********************
-         * 스택(Stack)
-         * 
-         * 선입후출(FILO), 후입선출(LIFO) 방식의 자료구조
-         * 가장 최신 입력된 순서대로 처리해야 하는 상황에 이용
-         ********************/
+        /******************************************************
+          * 스택 (Stack)
+          * 
+          * 선입후출(FILO), 후입선출(LIFO) 방식의 자료구조
+          * 가장 최신 입력된 순서대로 처리해야 하는 상황에 이용
+          ******************************************************/
 
-        void Test()
+        void Stack()
         {
             Stack<int> stack = new Stack<int>();
 
-            for (int i = 0; i < 10; i++) stack.Push(i);     // 0 1 2 3 4 5 6 7 8 9
+            for (int i = 0; i < 5; i++) stack.Push(i);  // 입력순서 : 0 1 2 3 4
 
-            Console.WriteLine(stack.Peek());                // 최상단 : 9
+            Console.WriteLine(stack.Peek());            // 최상단 : 4
 
-            while (stack.Count > 0) 
+            while (stack.Count > 0)
             {
-                Console.WriteLine(stack.Pop());             // 9 8 7 6 5 4 3 2 1 0
+                Console.WriteLine(stack.Pop());         // 출력순서 : 4 3 2 1 0
             }
         }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataStructure.Stack<int> stack = new DataStructure.Stack<int>();
+
+            for (int i = 0; i < 5; i++) stack.Push(i);  // 입력순서 : 0 1 2 3 4
+
+            Console.WriteLine(stack.Peek());            // 최상단 : 4
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());         // 출력순서 : 4 3 2 1 0
+            }
         }
     }
 }
