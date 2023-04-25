b52a69856eb5a2a364356999cbce2d7d1f6deedf
윤진 소 <pms010506@gmail.com>
2023-04-25 16:43:30 +0900

    feat: BinarySearchTree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BinarySearchTree
+﻿using System.Diagnostics;
+
+namespace BinarySearchTree
 {
     internal class Program
     {
@@ -25,6 +27,7 @@
 
         void BinarySearchTree()
         {
+            // 이진탐색트리는 그 특성상 정렬이 보장되어 있기에, SortedSet을 사용
             SortedSet<int> sortedSet = new SortedSet<int>();
 
             // 추가
@@ -40,11 +43,62 @@
 
             // 삭제
             sortedSet.Remove(3);
+
+            // key, value 이진탐색트리 (탐색용 키, 실제 데이터)
+            SortedDictionary<string, Monster> sortedDic = new SortedDictionary<string, Monster>();
+
+            sortedDic.Add("피카츄", new Monster() { name = "피카츄", hp = 100 });
+            sortedDic.Add("파이리", new Monster() { name = "피카츄", hp = 120 });
+            sortedDic.Add("꼬부기", new Monster() { name = "피카츄", hp = 80 });
+            sortedDic.Add("이상해씨", new Monster() { name = "피카츄", hp = 110 });
+            sortedDic.Add("리아코", new Monster() { name = "피카츄", hp = 130 });
+
+            Monster monster;
+            sortedDic.TryGetValue("파이리", out monster);      // 파이리 탐색 시도
+            Monster indexerMonster = sortedDic["파이리"];      // 인덱서를 통한 탐색
+
+            sortedDic.Remove("리아코");
+
+
+            // 이진탐색 검색효율
+            List<int> list = new List<int>();
+            SortedSet<int> set = new SortedSet<int>();
+
+            Random random = new Random();
+            int rand;
+            for (int i = 0; i < 1000000; i++)
+            {
+                rand = random.Next();
+                list.Add(rand);
+                set.Add(rand);
+            }
+            list[9999999] = -1;
+            set.Add(-1);
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            list.Find((x) => x == -1);
+            stopwatch.Stop();
+            Console.WriteLine("배열 time : {0}", stopwatch.ElapsedTicks);
+
+            stopwatch.Restart();
+            int value;
+            set.TryGetValue(-1, out value);
+            stopwatch.Stop();
+            Console.WriteLine("트리 time : {0}", stopwatch.ElapsedTicks);
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine();
+        }
 
+        class Monster
+        {
+            public string name;
+            public int hp;
+            public int mp;           
         }
     }
 }
