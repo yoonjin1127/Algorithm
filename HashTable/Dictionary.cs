d5a761c4e6956f01da897ae7285385eecf11da92
윤진 소 <pms010506@gmail.com>
2023-04-26 17:53:29 +0900

    feat: HashTable

---
+++ HashTable/Dictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Dictionary<TKey, TValue> where TKey : IEquatable<TKey>   // 비교가능한 값
+    {
+        private const int DefaultCapacity = 1000;
+
+        private struct Entry
+        {
+            public TKey key;
+            public TValue value;
+        }
+        private Entry[] table;
+
+        public Dictionary()
+        {
+            table = new Entry[DefaultCapacity];
+        }
+        public void Add(TKey key, TValue value) 
+        {
+            // 1. key를 index로 해싱
+            int index = key.GetHashCode() % table.Length;
+        }
+    }
+}
