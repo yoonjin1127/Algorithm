90570903b80c675f24bc957a2ffaded551b9acd4
윤진 소 <pms010506@gmail.com>
2023-04-27 17:16:54 +0900

    feat: DynamicProgramming

---
+++ DesignTechnique/DynamicProgramming.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignTechnique
+{
+    internal class DynamicProgramming
+    {
+        /********************
+         * 동적계획법 (Dynamic Programming)
+         * 
+         * 작은 문제의 해답을 큰 문제의 해답의 부분으로 이용하는 상향식 접근 방식
+         * 주어진 문제를 해결하기 위해 부분 문제에 대한 답을 계속적으로 활용해 나가는 기법
+         *********************/
+
+        // 동적계획법 <=> 분할접근
+        // 탐욕 알고리즘 <=> 백트래킹
+
+        int Fibonachi2(int x)
+        {
+            if (x == 0)
+                return 1;
+            if (x == 2)
+                return 1;
+            return Fibonachi2(x - 2) + Fibonachi2(x - 1);
+        }
+
+        // 예시 - 피보나치 수열
+        int Fibonachi(int x)
+        {
+            int[] fibonachi = new int[x + 1];
+            fibonachi[1] = 1;
+            fibonachi[2] = 1;
+
+            for (int i = 3; i <= x; i++)
+            {
+                fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+            }
+
+            return fibonachi[x];
+        }
+    }
+}
