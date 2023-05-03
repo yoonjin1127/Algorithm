7239f46639f39d4f6a692108a2ee47d1baf7d466
윤진 소 <sohyoonjin1127@gmail.com>
2023-05-03 12:51:03 +0900

    feat: ShortestPath

--- ShortestPath/Program.cs
+++ ShortestPath/Program.cs
@@ -2,7 +2,8 @@
 {
     internal class Program
     {
-        const int INF = 99999;
+        const int INF = 99999;      // 무한대 값
+
         static void Main(string[] args)
         {
             int[,] graph = new int[8, 8]    // 0~7까지의 정점
@@ -14,16 +15,52 @@
             // 0에서 6까지의 거리는 10
             // 0에서 7까지의 거리는 11
                 {
-                     { 0, 6, 14, 7, 20, 17, 10, 11},
-                     { 6, 0, 15  },
-                     { 0,  },
-                     { 0,  },
-                     { 0,  },
-                     { 0,  },
-                     { 0,  },
-                     { 0,  }
+                     {  0,  6, 14,  7, 20, 17, 10, 11 },
+                     {  6,  0,  8,  7, INF, INF, INF, INF },
+                     { 14,  8,  0, INF,  6,  6, INF, INF },
+                     {  7,  7, INF,  0, INF, INF, INF,  8 },
+                     { 20, INF,  6, INF,  0, INF, INF, INF },
+                     { 17, INF,  6, INF, INF,  0, INF,  6 },
+                     { 10, INF, INF, INF, INF, INF,  0,  1 },
+                     { 11, INF, INF,  8, INF,  6,  1,  0 },
 
                 };
+
+            int[] distance;     // 거리
+            int[] path;         // 경로
+
+            //  다익스트라 알고리즘을 이용한 최단 경로 계산
+            Dijkstra.ShortestPath(in graph, 0, out distance, out path);
+            Console.WriteLine("<Dijkstra>");
+            PrintDijkstra(distance, path);
+
+            Console.WriteLine();
+
+
+        }
+
+        private static void PrintDijkstra(int[] distance, int[] path)
+        {
+            Console.Write("Vertex");
+            Console.Write("\t");
+            Console.Write("dist");
+            Console.Write("\t");
+            Console.WriteLine("path");
+
+            for (int i = 0; i < distance.Length; i++)
+            {
+                Console.Write("{0,3}", i);
+                Console.Write("\t");
+                if (distance[i] >= INF)
+                    Console.Write("INF");
+                else
+                    Console.Write("{0,3}", distance[i]);
+                Console.Write("\t");
+                if (path[i] < 0)
+                    Console.WriteLine("  X ");
+                else
+                    Console.WriteLine("{0,3}", path[i]);
+            }
         }
     }
 }
